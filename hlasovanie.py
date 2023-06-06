3190c2d369278c2cb1e125f43b60cf070e1c70aa
ukfIG2 <126603732+ukfIG2@users.noreply.github.com>
2023-06-07 01:18:50 +0200

    Update hlasovanie.py
    Doplnenie komentov, pokus o sprehladnenie.

--- hlasovanie.py
+++ hlasovanie.py
@@ -1,5 +1,6 @@
 import pandas as pd
 try:
+    #Premenné
     poradie = ["1.","2.","3.","4.","5.","6.","7."]
     strany = ["SMER","REPUBLIKA","OLANO","SAS","SME-RODINA","Progresivci","Hlas"]
     vyherne_strany = ["Hlas", "Progresivci", "SAS", "SMER", "Republika", "Sme-rodina", "Olano"]
@@ -40,14 +41,17 @@ try:
     #Vytvoríme tabuľku, aby to bolo prehladnejšie.
     tabulka = pd.DataFrame({"Falošné poradie strán":vyherne_strany, "Zfalšované počty hlasov":falosne_hlasy, "Reálne hlasovanie zfalšovaného hlasovania":realne_hlasovanie, "xxx":xxx, "Realné poradie":realne_strany, "Realne hlasy":realne_hlasy})
     print()     #Odriadkovanie
-    print(tabulka.to_string())
+    print(tabulka.to_string())      #Vytlačí tabuľku
     print()     #Odriadkovanie
     print("4 377 565 je počet voličov z referenda 2023. Voľby do NRSR 2020 bolo 4 432 419.")
+    
     if hlasovalo>4377565:       #Ak ste zadali viac ako 4 377 565 hlasov tak percentá sú zbytočné. 
         print("Dali ste vačší počet hlasov ako 4 377 565. Dali ste "+str(hlasovalo)+" hlasov.")
     else:                       #Percento hlasovania z celkových voličov z referenda 2023.
         print("Hlasovalo "+str(hlasovalo)+" z 4 377 565. "+str(round(((hlasovalo/4377565)*100),2))+"% volilo.")
-    print("\nAk to vypadá zle, zmenši si to 'ctr' + '-' alebo zvacsi 'ctr' + '+'.")
+    print("\nAk to vypadá zle, zmenšite si to 'ctr' + '-' alebo zvačsite si to 'ctr' + '+'.")
+
+#Spustí sa v prípade číselnej chyby.
 except ValueError as Err:
     print()     #Odriadkovanie
     print("Zadaj cisla bez medzie napr.1000000000")
