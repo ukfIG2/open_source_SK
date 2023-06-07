d36f44285dd4974e08dd2dc8f06ab44ebe96db2a
ukfIG2 <126603732+ukfIG2@users.noreply.github.com>
2023-06-07 13:49:01 +0200

    Update hlasovanie.py
    Pokus o sprehladnenie.

--- hlasovanie.py
+++ hlasovanie.py
@@ -3,29 +3,29 @@ try:
     #Premenné
     poradie = ["1.","2.","3.","4.","5.","6.","7."]
     strany = ["SMER","REPUBLIKA","OLANO","SAS","SME-RODINA","Progresivci","Hlas"]
-    vyherne_strany = ["Hlas", "Progresivci", "SAS", "SMER", "Republika", "Sme-rodina", "Olano"]
+    vyherne_strany = ["Hlas","Progresivci","SAS","SMER","Republika","Sme-rodina","Olano"]
     hlasy = [0,0,0,0,0,0,0]
     falosne_hlasy = []
     realne_strany = []
     realne_hlasy = []
     realne_hlasovanie = [0,0,0,0,0,0,0]
-    xxx = ["xxx", "xxx", "xxx", "xxx", "xxx", "xxx", "xxx"]
+    xxx = ["xxx","xxx","xxx","xxx","xxx","xxx","xxx"]
 
     #Nazbierame počet hlasov pre konkrétnu stranu.
     for i in range(len(strany)):
-        hlasy[i] = int(input("Zadaj pocet hlasov pre stranu "+ strany[i]+": "))
+        hlasy[i] = int(input("Zadaj pocet hlasov pre stranu " + strany[i] + ": "))
 
     #Zoradíme zoznam "falosne_hlasy" od najvyššej hodnoty po najmenšiu.
     falosne_hlasy = sorted(hlasy, reverse=True)
 
     #Falošným výsledkom priradíme reálne počty hlasov.
-    realne_hlasovanie[0]=hlasy[6]
-    realne_hlasovanie[1]=hlasy[5]
-    realne_hlasovanie[2]=hlasy[3]
-    realne_hlasovanie[3]=hlasy[0]
-    realne_hlasovanie[4]=hlasy[1]
-    realne_hlasovanie[5]=hlasy[4]
-    realne_hlasovanie[6]=hlasy[2]
+    realne_hlasovanie[0] = hlasy[6]
+    realne_hlasovanie[1] = hlasy[5]
+    realne_hlasovanie[2] = hlasy[3]
+    realne_hlasovanie[3] = hlasy[0]
+    realne_hlasovanie[4] = hlasy[1]
+    realne_hlasovanie[5] = hlasy[4]
+    realne_hlasovanie[6] = hlasy[2]
 
     #Vypočítame celkový počet hlasov.
     hlasovalo = sum(realne_hlasovanie)
@@ -46,9 +46,9 @@ try:
     print("4 377 565 je počet voličov z referenda 2023. Voľby do NRSR 2020 bolo 4 432 419.")
     
     if hlasovalo>4377565:       #Ak ste zadali viac ako 4 377 565 hlasov tak percentá sú zbytočné. 
-        print("Dali ste vačší počet hlasov ako 4 377 565. Dali ste "+str(hlasovalo)+" hlasov.")
+        print("Dali ste vačší počet hlasov ako 4 377 565. Dali ste " + str(hlasovalo) + " hlasov.")
     else:                       #Percento hlasovania z celkových voličov z referenda 2023.
-        print("Hlasovalo "+str(hlasovalo)+" z 4 377 565. "+str(round(((hlasovalo/4377565)*100),2))+"% volilo.")
+        print("Hlasovalo " + str(hlasovalo) + " z 4 377 565. " + str(round(((hlasovalo/4377565)*100),2)) + "% volilo.")
     print("\nAk to vypadá zle, zmenšite si to 'ctr' + '-' alebo zvačsite si to 'ctr' + '+'.")
 
 #Spustí sa v prípade číselnej chyby.
